65495b038369dcb6b051ac5af6b79cc2282489e2
Chris Liu <acciochris@outlook.com>
2023-11-05 12:34:40 -0800

    Remove server in main

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 from machine import Pin
-import server
 
 # pins
-D5 = Pin(14, Pin.IN, Pin.PULL_UP)  # switch 1: app mode
-D6 = Pin(12, Pin.IN, Pin.PULL_UP)  # swirch 4: modify program
-LED = Pin(2, Pin.OUT)
+D6 = Pin(12, Pin.IN, Pin.PULL_UP)  # modify program
 
 # stop everything to flash on D2 == 0
 if D6.value() != 0:
-    if D5.value() == 0:
-        # run file server
-        LED.on()
-        server.setup()
-        server.run()
-    else:
-        # run app
-        LED.off()
-        import app
+    import app
 
-        app.run()
+    app.run()
+else:
+    LED = Pin(2, Pin.OUT)
+    LED.off()
