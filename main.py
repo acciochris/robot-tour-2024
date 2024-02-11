cffc6e8d4dba623dbda42b3b18d9894dc1be33b5
Chris Liu <acciochris@outlook.com>
2024-02-10 17:56:25 -0800

    Change pin for program modification

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from machine import Pin
 
 # pins
-D6 = Pin(12, Pin.IN, Pin.PULL_UP)  # modify program
+P = Pin(13, Pin.IN, Pin.PULL_UP)  # modify program
 
 # stop everything to flash on D2 == 0
-if D6.value() != 0:
+if P.value() != 0:
     import app
 
     app.run()
